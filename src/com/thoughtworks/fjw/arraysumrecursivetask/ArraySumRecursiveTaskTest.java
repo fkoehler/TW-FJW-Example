7d2db9f239e1a200b7d2526693eac385e4d06689
Fabian Koehler <fab@fabiankoehler.de>
2011-09-05 00:26:33 +0200

    new build.properties update examples from current state

--- src/com/thoughtworks/fjw/arraysumrecursivetask/ArraySumRecursiveTaskTest.java
+++ src/com/thoughtworks/fjw/arraysumrecursivetask/ArraySumRecursiveTaskTest.java
@@ -1,11 +1,13 @@
 package com.thoughtworks.fjw.arraysumrecursivetask;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import java.util.concurrent.ForkJoinPool;
 
 import org.junit.Test;
+import org.perf4j.LoggingStopWatch;
+import org.perf4j.StopWatch;
 
 import com.thoughtworks.fjw.utils.Utils;
 
@@ -15,13 +17,81 @@ public class ArraySumRecursiveTaskTest {
 	public void shouldCalculateTheSumOfAllArrayElements() {
 		int[] arrayToCalculateSumOf = Utils.buildRandomIntArray();
 
-		ArraySumRecursiveTask arraySumCalculator = new ArraySumRecursiveTask(arrayToCalculateSumOf);
-		ForkJoinPool forkJoinPool = new ForkJoinPool();
-		long result = forkJoinPool.invoke(arraySumCalculator);
+		StopWatch stopWatch = new LoggingStopWatch("singlethread");
+		int expected = 0;
+		for (int value : arrayToCalculateSumOf) {
+			Utils.doCpuIntensiveCalculation();
 
-		long expected = Utils.calculateSumOfArray(arrayToCalculateSumOf);
+			expected += value;
+		}
+		stopWatch.stop();
+
+		stopWatch = new LoggingStopWatch("multithread");
+		ArraySumRecursiveTask arraySumCalculator = new ArraySumRecursiveTask(arrayToCalculateSumOf);
+
+		int nofProcessors = Runtime.getRuntime().availableProcessors();
+
+		ForkJoinPool forkJoinPool = new ForkJoinPool(nofProcessors);
+
+		int result = forkJoinPool.invoke(arraySumCalculator);
+		stopWatch.stop();
 
 		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testThatCompleteTaskWorksAsExpected() {
+		int[] arrayToCalculateSumOf = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // = 55
+
+		ArraySumRecursiveTask arraySumRecursiveTask = new ArraySumRecursiveTask(arrayToCalculateSumOf);
+		ForkJoinPool pool = new ForkJoinPool();
+		int actual = pool.invoke(arraySumRecursiveTask);
+
+		assertEquals(55, actual);
+	}
+
+	@Test
+	public void testThatCoreComputationBehavesAsExpected() {
+		int[] arrayToCalculateSumOf = new int[] { 5 };
+		ArraySumRecursiveTask arraySumRecursiveTask = new ArraySumRecursiveTask(arrayToCalculateSumOf);
+
+		assertEquals(5, arraySumRecursiveTask.doCoreComputation());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testThatCoreComputationOnlyCalculatesOneElement() {
+		int[] arrayToCalculateSumOf = new int[] { 5, 6 };
+		ArraySumRecursiveTask arraySumRecursiveTask = new ArraySumRecursiveTask(arrayToCalculateSumOf);
+		arraySumRecursiveTask.doCoreComputation();
+	}
+
+	@Test
+	public void testSmallestWorkingUnit() {
+		int[] arrayToCalculateSumOf = new int[] { 5 };
+		ArraySumRecursiveTask arraySumRecursiveTask = new ArraySumRecursiveTask(arrayToCalculateSumOf);
+
+		assertEquals(new Integer(5), arraySumRecursiveTask.compute());
+	}
+
+	@Test
+	public void testThatSplittingTheTaskWorksAsExpected() {
+		int[] arrayToCalculateSumOf = new int[] { 1, 2, 3 };
+		ArraySumRecursiveTask arraySumRecursiveTask = new ArraySumRecursiveTask(arrayToCalculateSumOf);
+
+		assertArrayEquals(new int[][] { { 1 }, { 2, 3 } }, arraySumRecursiveTask.splitArrayInParts());
+	}
+
+	@Test
+	public void testThatSplittingTheTaskWorksAsExpecteForOneArrayElement() {
+		int[] arrayToCalculateSumOf = new int[] { 1 };
+		ArraySumRecursiveTask arraySumRecursiveTask = new ArraySumRecursiveTask(arrayToCalculateSumOf);
+
+		assertArrayEquals(new int[][] { {}, { 1 } }, arraySumRecursiveTask.splitArrayInParts());
+	}
+
+	@Test
+	public void testThatMergingResultBehavesAsExpected() {
+		assertEquals(6, ArraySumRecursiveTask.mergeResults(1, 5));
 	}
 
 }
--- src/com/thoughtworks/fjw/utils/TimeKeeper.java
+++ src/com/thoughtworks/fjw/utils/TimeKeeper.java
@@ -1,13 +1,55 @@
 package com.thoughtworks.fjw.utils;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
 public class TimeKeeper {
-	static final String SEPARATOR = " | ";
 
-	public static void logTimes(final Logger logger, final String context, final long threadId, final long startTime,
-			final long stopTime) {
+	static final String SEPARATOR = ";";
+	private static final String DEFAULT_PATTERN = "-ParallelJavaWorkshop.csv";
+	private static final String N_A = "n/a";
+
+	private static FileHandler fileHandler;
+
+	public TimeKeeper() throws SecurityException, IOException {
+		this(getLogFileName());
+	}
+
+	private static String getLogFileName() {
+		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
+		String dateTime = df.format(new Date());
+
+		return sanitizeFilename(dateTime) + DEFAULT_PATTERN;
+	}
+
+	private static String sanitizeFilename(final String name) {
+		return name.replaceAll("[ :\\\\/*?|<>]", "_");
+	}
+
+	public TimeKeeper(final String aPattern) throws SecurityException, IOException {
+		if (fileHandler != null) {
+			fileHandler.close();
+		}
+
+		fileHandler = new FileHandler(aPattern);
+		fileHandler.setFormatter(new StrFormatter());
+	}
+
+	public void addFileHandlerToLogger(final Logger aLogger) throws SecurityException, IOException {
+		aLogger.addHandler(fileHandler);
+	}
+
+	public static FileHandler getFileHandler() {
+		return fileHandler;
+	}
+
+	public static String createLogMessage(final String context, final long threadId, final long startTime,
+			final long stopTime, final LogCode aCode) {
 		StringBuilder builder = new StringBuilder();
+
 		builder.append(context);
 		builder.append(SEPARATOR);
 		builder.append(threadId);
@@ -17,18 +59,48 @@ public class TimeKeeper {
 		builder.append(stopTime);
 		builder.append(SEPARATOR);
 		builder.append(stopTime - startTime);
-		logger.info(builder.toString());
+		builder.append(SEPARATOR);
+		builder.append(aCode);
+
+		return builder.toString();
 	}
 
-	public static void logTimes(final Logger logger, final String context, final long threadId, final long startTime,
-			final ActionCode code) {
+	public static String createLogTitle(final String contextTitle, final String threadIdTitle,
+			final String startTimeTitle, final String stopTimeTitle, final String diffTitle, final String aCodeTitle) {
 		StringBuilder builder = new StringBuilder();
+
+		builder.append(contextTitle);
+		builder.append(SEPARATOR);
+		builder.append(threadIdTitle);
+		builder.append(SEPARATOR);
+		builder.append(startTimeTitle);
+		builder.append(SEPARATOR);
+		builder.append(stopTimeTitle);
+		builder.append(SEPARATOR);
+		builder.append(diffTitle);
+		builder.append(SEPARATOR);
+		builder.append(aCodeTitle);
+
+		return builder.toString();
+	}
+
+	public static String createLogMessage(final String context, final long threadId, final long startTime,
+			final LogCode aCode) {
+		StringBuilder builder = new StringBuilder();
+
 		builder.append(context);
 		builder.append(SEPARATOR);
 		builder.append(threadId);
 		builder.append(SEPARATOR);
 		builder.append(startTime);
-		logger.info(builder.toString());
+		builder.append(SEPARATOR);
+		builder.append(N_A);
+		builder.append(SEPARATOR);
+		builder.append(N_A);
+		builder.append(SEPARATOR);
+		builder.append(aCode);
+
+		return builder.toString();
 	}
 
 }
